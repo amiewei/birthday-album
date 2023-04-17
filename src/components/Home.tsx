81c0cc2c24ca06d6f33a65ad2d668ee441ed47b1
Amie Wei <amie.wei12@gmail.com>
2023-04-16 21:39:26 -0500

    updated file

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import Gallery from "./Album";
 export function Home() {
   return (
     <section className="bg-emerald-800 px-4 py-20 dark:bg-gray-900 lg:py-36 lg:pb-2">
-      <div className="mx-auto grid px-2 py-4 pb-20 lg:max-w-screen-xl lg:px-8 xl:gap-4">
+      <div className="mx-auto grid min-h-[70vh] px-2 py-4 pb-20 lg:max-w-screen-xl lg:px-8 xl:gap-4">
         <h1 className="text-4xl text-white lg:text-8xl">Celebrating Кирил </h1>
         <h1 className="text-white lg:text-2xl">May 15th, 2023 - Chicago, IL</h1>
         <img
